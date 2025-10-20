cc7254f67a39204a6e7e3adc14fb1719a88b0e38
seethala e v <sheethalev@gmail.com>
2025-10-20 17:17:12 +0530

    inspected

--- src/test/java/Base/Bases.java
+++ src/test/java/Base/Bases.java
@@ -1,9 +1,21 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 
-public class Bases {
+public class Bases 
+{
 
 	WebDriver driver;
+	
+	@BeforeMethod()
+	public void before()
+	{
+		By email=By.id("//*[@id=\"email\"]");
+		By pas=By.id("//*[@id=\"password\"]");
+	}
+	
 	
 }
