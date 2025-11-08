49fdfe3047b86fbccc03b4749b4c403c6613eef9
seethala e v <sheethalev@gmail.com>
2025-11-08 23:08:01 +0530

    values given through sendkeys

--- src/test/java/Base/Products.java
+++ src/test/java/Base/Products.java
@@ -37,4 +37,36 @@ public class Products
 	{
 		this.driver=driver;	
 	}
+	public void acton(String name,String cardno,String cvno,String expir,String expyear)
+	{
+		driver.findElement(product).click();
+		
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,500)","");
+		driver.findElement(target).click();
+		driver.findElement(cont).click();
+		driver.findElement(view).click();
+		driver.findElement(arrowmark);
+		driver.findElement(addcart).click();
+		driver.findElement(viewc).click();
+		driver.findElement(itemdeselect).click();
+		driver.findElement(procheckout).click();
+		driver.findElement(concart).click();
+		driver.findElement(procheckout).click();
+		driver.findElement(login).click();
+		driver.findElement(cart).click();
+		driver.findElement(Procheck).click();
+		driver.findElement(orderplace).click();
+		driver.findElement(namecard).sendKeys(name);
+		driver.findElement(card).sendKeys(cardno);
+		driver.findElement(cv).sendKeys(cvno);
+		driver.findElement(expiry).sendKeys(expir);
+		driver.findElement(expiryyear).sendKeys(expyear);
+		driver.findElement(payorder).click();
+		driver.findElement(ordeplace).click();
+		driver.findElement(down).click();
+		driver.findElement(con).click();
+
+			}
+}
 	
