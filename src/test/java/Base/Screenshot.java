66d8662cde90acb798385d70afa89467532b4b13
seethala e v <sheethalev@gmail.com>
2025-11-09 22:54:08 +0530

    scrrenshot

--- src/test/java/Base/Screenshot.java
+++ src/test/java/Base/Screenshot.java
@@ -12,35 +12,3 @@ import org.openqa.selenium.io.FileHandler;
 
 public class Screenshot 
 {
-	WebDriver driver;
-	By testcas=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a");
-	By img=By.xpath("//*[@id=\"header\"]/div/div/div/div[1]/div/a/img");
-	
-	public Screenshot(WebDriver driver)
-	{
-		this.driver=driver;
-	}
-	public void scr() throws IOException
-	{
-		driver.findElement(testcas).click();
-		
-		JavascriptExecutor js=(JavascriptExecutor)driver;
-		js.executeScript("window.scrollBy(0,1000)","");
-		File sreen=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		File dest=new File("D://E54u.png");
-		FileHandler.copy(sreen, dest);
-		driver.navigate().back();
-		driver.navigate().refresh();
-		driver.navigate().to("https://automationexercise.com/");
-		Boolean imag=driver.findElement(img).isDisplayed();
-		if(imag)
-		{
-			System.out.println("image displayed");
-		}
-		else
-		{
-			System.out.println("image not displayed");
-		}
-
-			}
-}
