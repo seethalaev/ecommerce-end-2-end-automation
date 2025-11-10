701ae63c134f34bfdb40a7e6f2c41b6f4f9acdf5
seethala e v <sheethalev@gmail.com>
2025-11-10 23:29:03 +0530

    inspected,constructor

--- src/test/java/Base/Login.java
+++ src/test/java/Base/Login.java
@@ -25,4 +25,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 	    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
 
 	
-	
+	    package Base;
+
+	    import java.io.IOException;
+	    import java.net.HttpURLConnection;
+	    import java.net.MalformedURLException;
+	    import java.net.URL;
+	    import java.time.Duration;
+	    import java.util.List;
+
+	    import org.openqa.selenium.By;
+	    import org.openqa.selenium.WebDriver;
+	    import org.openqa.selenium.WebElement;
+	    import org.openqa.selenium.support.ui.ExpectedConditions;
+	    import org.openqa.selenium.support.ui.WebDriverWait;
+
+	       public class Login 
+
+	       {
+
+	    		WebDriver driver;
+	    		WebDriverWait wait;
+	    		By email=By.xpath("/html/body/section/div/div/div[1]/div/form/input[2]");
+	    		By pas=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]");
+	    		By login=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button");
+	    	    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
+
+	    	
+	    		public Login(WebDriver driver)
+	    		{
+	    			this.driver = driver;
+	    			wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+
+	    		}
+	    
