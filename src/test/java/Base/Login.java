2526ab671a500c5e3fb69e70868e0911bbfd4d2f
seethala e v <sheethalev@gmail.com>
2025-11-10 23:34:17 +0530

    inspected,constructor,login,content verify,invalidusername,valid password,valid username,invald password

--- src/test/java/Base/Login.java
+++ src/test/java/Base/Login.java
@@ -25,199 +25,105 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 	    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
 
 	
-	    package Base;
+		public Login(WebDriver driver)
+		{
+			this.driver = driver;
+			wait=new WebDriverWait(driver,Duration.ofSeconds(10));
 
-	    import java.io.IOException;
-	    import java.net.HttpURLConnection;
-	    import java.net.MalformedURLException;
-	    import java.net.URL;
-	    import java.time.Duration;
-	    import java.util.List;
-
-	    import org.openqa.selenium.By;
-	    import org.openqa.selenium.WebDriver;
-	    import org.openqa.selenium.WebElement;
-	    import org.openqa.selenium.support.ui.ExpectedConditions;
-	    import org.openqa.selenium.support.ui.WebDriverWait;
-
-	       public class Login 
-
-	       {
-
-	    		WebDriver driver;
-	    		WebDriverWait wait;
-	    		By email=By.xpath("/html/body/section/div/div/div[1]/div/form/input[2]");
-	    		By pas=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]");
-	    		By login=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button");
-	    	    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
-
-	    	
-	    		public Login(WebDriver driver)
-	    		{
-	    			this.driver = driver;
-	    			wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-
-	    		}
-	    		package Base;
-
-	    		import java.io.IOException;
-	    		import java.net.HttpURLConnection;
-	    		import java.net.MalformedURLException;
-	    		import java.net.URL;
-	    		import java.time.Duration;
-	    		import java.util.List;
-
-	    		import org.openqa.selenium.By;
-	    		import org.openqa.selenium.WebDriver;
-	    		import org.openqa.selenium.WebElement;
-	    		import org.openqa.selenium.support.ui.ExpectedConditions;
-	    		import org.openqa.selenium.support.ui.WebDriverWait;
-
-	    		   public class Login 
-
-	    		   {
-
-	    				WebDriver driver;
-	    				WebDriverWait wait;
-	    				By email=By.xpath("/html/body/section/div/div/div[1]/div/form/input[2]");
-	    				By pas=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]");
-	    				By login=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button");
-	    			    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
-
-	    			
-	    				public Login(WebDriver driver)
-	    				{
-	    					this.driver = driver;
-	    					wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-
-	    				}
-	    			public void login(String mail,String pswd) throws IOException
-	    			{
-	    				String actuapgsrc=driver.getPageSource();
-	    				if(actuapgsrc.contains(" Video Tutorials"))
-	    				{
-	    					System.out.println("video editor is in as content");
-	    				}
-	    				else
-	    				{
-	    					System.out.println("content not present");
-	    				}
-	    				wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(mail);
-	    		        wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(pswd);
-	    		        wait.until(ExpectedConditions.elementToBeClickable(login)).click();
-	    		        wait.until(ExpectedConditions.elementToBeClickable(logout)).click();
-	    		        String base=driver.getCurrentUrl();
-	    		        String actual=driver.getTitle();
-	    		        System.out.println("title "+actual);
-	    		        package Base;
-
-	    		        import java.io.IOException;
-	    		        import java.net.HttpURLConnection;
-	    		        import java.net.MalformedURLException;
-	    		        import java.net.URL;
-	    		        import java.time.Duration;
-	    		        import java.util.List;
-
-	    		        import org.openqa.selenium.By;
-	    		        import org.openqa.selenium.WebDriver;
-	    		        import org.openqa.selenium.WebElement;
-	    		        import org.openqa.selenium.support.ui.ExpectedConditions;
-	    		        import org.openqa.selenium.support.ui.WebDriverWait;
-
-	    		           public class Login 
-
-	    		           {
-
-	    		        		WebDriver driver;
-	    		        		WebDriverWait wait;
-	    		        		By email=By.xpath("/html/body/section/div/div/div[1]/div/form/input[2]");
-	    		        		By pas=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]");
-	    		        		By login=By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/button");
-	    		        	    By logout=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
-
-	    		        	
-	    		        		public Login(WebDriver driver)
-	    		        		{
-	    		        			this.driver = driver;
-	    		        			wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-
-	    		        		}
-	    		        	public void login(String mail,String pswd) throws IOException
-	    		        	{
-	    		        		String actuapgsrc=driver.getPageSource();
-	    		        		if(actuapgsrc.contains(" Video Tutorials"))
-	    		        		{
-	    		        			System.out.println("video editor is in as content");
-	    		        		}
-	    		        		else
-	    		        		{
-	    		        			System.out.println("content not present");
-	    		        		}
-	    		        		wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(mail);
-	    		                wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(pswd);
-	    		                wait.until(ExpectedConditions.elementToBeClickable(login)).click();
-	    		                wait.until(ExpectedConditions.elementToBeClickable(logout)).click();
-	    		                String base=driver.getCurrentUrl();
-	    		                String actual=driver.getTitle();
-	    		                System.out.println("title "+actual);
-	    		               
-	    		                if(actual.equalsIgnoreCase("https://automationexercise.com/"))
-	    		                {
-	    		                	System.out.println("title is same");
-	    		                }
-	    		                else
-	    		                {
-	    		                	System.out.println("title is not same");
-	    		                	driver.findElement(email).clear();
-	    		                	driver.findElement(pas).clear();
-	    		                }
-	    		                
-	    		                
-	    		        		URL ob=new URL(base);
-	    		        		HttpURLConnection con =(HttpURLConnection)ob.openConnection();
-	    		        		int responsecode=con.getResponseCode();
-	    		        		System.out.println("responsecode "+responsecode);
-	    		        		con.connect();
-	    		        		if(con.getResponseCode()==200)
-	    		        		{
-	    		        			System.out.println("valid response code");
-	    		        		}
-	    		        		else
-	    		        		{
-	    		        			System.out.println("invalid response code");
-	    		        		}
-	    		        	}
-	    		        	public void linkcount()
-	    		        	{
-	    		        		List<WebElement> li = driver.findElements(By.tagName("a"));
-	    		        		for(WebElement s:li)
-	    		        		{
-	    		        			String link=s.getAttribute("href");
-	    		        			System.out.println("link "+link);
-	    		        		}
-	    		        	}
-	    		        	
-	    		        		
-	    		        	
-	    		        	public void InUnamepass(String mil,String paswd)
-	    		         	{
-	    		        		wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(mil);
-	    		                wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(paswd);
-	    		                wait.until(ExpectedConditions.elementToBeClickable(login)).click();
-	    		                String actual=driver.getCurrentUrl();
-	    		                System.out.println(actual+"url");
-	    		                if(actual.equalsIgnoreCase("https://automationexercise.com/"))
-	    		                {
-	    		                	System.out.println("title is same");
-	    		                }
-	    		                else
-	    		                {
-	    		                	System.out.println("title is not same");
-	    		                	driver.findElement(email).clear();
-	    		                	driver.findElement(pas).clear();
-	    		            
-	    		                }
-	    		         	}
-	    		              
-
-	    		     
+		}
+	public void login(String mail,String pswd) throws IOException
+	{
+		String actuapgsrc=driver.getPageSource();
+		if(actuapgsrc.contains(" Video Tutorials"))
+		{
+			System.out.println("video editor is in as content");
+		}
+		else
+		{
+			System.out.println("content not present");
+		}
+		wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(mail);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(pswd);
+        wait.until(ExpectedConditions.elementToBeClickable(login)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(logout)).click();
+        String base=driver.getCurrentUrl();
+        String actual=driver.getTitle();
+        System.out.println("title "+actual);
+       
+        if(actual.equalsIgnoreCase("https://automationexercise.com/"))
+        {
+        	System.out.println("title is same");
+        }
+        else
+        {
+        	System.out.println("title is not same");
+        	driver.findElement(email).clear();
+        	driver.findElement(pas).clear();
+        }
+        
+        
+		URL ob=new URL(base);
+		HttpURLConnection con =(HttpURLConnection)ob.openConnection();
+		int responsecode=con.getResponseCode();
+		System.out.println("responsecode "+responsecode);
+		con.connect();
+		if(con.getResponseCode()==200)
+		{
+			System.out.println("valid response code");
+		}
+		else
+		{
+			System.out.println("invalid response code");
+		}
+	}
+	public void linkcount()
+	{
+		List<WebElement> li = driver.findElements(By.tagName("a"));
+		for(WebElement s:li)
+		{
+			String link=s.getAttribute("href");
+			System.out.println("link "+link);
+		}
+	}
+	
+		
+	
+	public void InUnamepass(String mil,String paswd)
+ 	{
+		wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(mil);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(paswd);
+        wait.until(ExpectedConditions.elementToBeClickable(login)).click();
+        String actual=driver.getCurrentUrl();
+        System.out.println(actual+"url");
+        if(actual.equalsIgnoreCase("https://automationexercise.com/"))
+        {
+        	System.out.println("title is same");
+        }
+        else
+        {
+        	System.out.println("title is not same");
+        	driver.findElement(email).clear();
+        	driver.findElement(pas).clear();
+    
+        }
+ 	}
+        public void UnameInpass(String maail,String paswd)
+        {
+        	wait.until(ExpectedConditions.visibilityOfElementLocated(email)).sendKeys(maail);
+        	wait.until(ExpectedConditions.visibilityOfElementLocated(pas)).sendKeys(paswd);
+        	wait.until(ExpectedConditions.elementToBeClickable(login)).click();
+        	 String actual=driver.getCurrentUrl();
+             if(actual.equalsIgnoreCase("https://automationexercise.com/"))
+             {
+             	System.out.println("title is same");
+             }
+             else
+             {
+             	System.out.println("title is not same");
+            	driver.findElement(email).clear();
+            	driver.findElement(pas).clear();
+        
+             }
+        }
+	
+}
