f07cb5f6e3243f48c6008be0da2be1e74612fe24
seethala e v <sheethalev@gmail.com>
2025-11-11 23:08:45 +0530

    created delete,inspected

---
+++ src/test/java/Base/Delete.java
@@ -0,0 +1,16 @@
+package Base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Delete
+{
+	WebDriver driver;
+	By delete=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a/i");
+	
+
+
