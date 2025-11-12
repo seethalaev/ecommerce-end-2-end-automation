a85be21df9ed94069e3380ef16d94ded9a28f725
seethala e v <sheethalev@gmail.com>
2025-11-12 23:12:14 +0530

    created contactus,inspected

---
+++ src/test/java/Base/Contactus.java
@@ -0,0 +1,26 @@
+package Base;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Contactus {
+
+	WebDriver driver;
+	By contactus=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[8]/a");
+	By name=By.xpath("//*[@id=\"contact-us-form\"]/div[1]/input");
+	By email=By.xpath("//*[@id=\"contact-us-form\"]/div[2]/input");
+	By sub=By.xpath("//*[@id=\"contact-us-form\"]/div[3]/input");
+	By message=By.xpath("//*[@id=\"message\"]");
+	By choosefile=By.name("upload_file");
+	By subit=By.name("submit");
+	By msg=By.xpath("//*[@id=\"contact-page\"]/div[2]/div[1]/div/div[2]");
+	
+	
