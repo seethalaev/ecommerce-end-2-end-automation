e4653ca1153ce9b4ac0c34e5ff59656a2efe36aa
seethala e v <sheethalev@gmail.com>
2025-11-13 23:01:43 +0530

    products created,webdriver inspected,xpath created

--- src/test/java/Base/Products.java
+++ src/test/java/Base/Products.java
@@ -7,6 +7,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class Products
-{
-	WebDriver driver;
-	
+
+	{
+		WebDriver driver;
+		By product=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
+		By target = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/a");
+		By cont=By.xpath("//*[@id=\"cartModal\"]/div/div/div[3]/button");
+		By view=By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[2]/ul/li/a/i");
+		By arrowmark=By.xpath("//*[@id=\"quantity\"]");
+		By addcart=By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/span/button");
+		By viewc=By.xpath("//*[@id=\"cartModal\"]/div/div/div[2]/p[2]/a/u");
+		
