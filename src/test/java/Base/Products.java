1e4f67bea499649cfb08d6384c9e3f625310864e
seethala e v <sheethalev@gmail.com>
2025-11-13 23:00:59 +0530

    products created,webdriver inspected

---
+++ src/test/java/Base/Products.java
@@ -0,0 +1,12 @@
+package Base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class Products
+{
+	WebDriver driver;
+	
