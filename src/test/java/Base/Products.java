04f13d0e4ba0289943ed0dff83755d1e0ee41ea1
seethala e v <sheethalev@gmail.com>
2025-11-13 23:02:48 +0530

    products created,webdriver inspected,xpath created,constructor createed

--- src/test/java/Base/Products.java
+++ src/test/java/Base/Products.java
@@ -7,14 +7,34 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class Products
-
+{
+	WebDriver driver;
+	By product=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
+	By target = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/a");
+	By cont=By.xpath("//*[@id=\"cartModal\"]/div/div/div[3]/button");
+	By view=By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[2]/ul/li/a/i");
+	By arrowmark=By.xpath("//*[@id=\"quantity\"]");
+	By addcart=By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/span/button");
+	By viewc=By.xpath("//*[@id=\"cartModal\"]/div/div/div[2]/p[2]/a/u");
+	By itemdeselect=By.xpath("//*[@id=\"product-2\"]/td[6]/a/i");
+	By procheckout=By.xpath("//*[@id=\"do_action\"]/div[1]/div/div/a");
+	By concart=By.xpath("//*[@id=\"checkoutModal\"]/div/div/div[3]/button");
+	By login=By.xpath("//*[@id=\"checkoutModal\"]/div/div/div[2]/p[2]/a/u");
+	By cart=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[3]/a");
+	By Procheck=By.xpath("//*[@id=\"do_action\"]/div[1]/div/div/a");
+	By orderplace=By.xpath("//*[@id=\"cart_items\"]/div/div[7]/a");
+	By namecard=By.xpath("//*[@id=\"payment-form\"]/div[1]/div/input");
+	By card=By.xpath("//*[@id=\"payment-form\"]/div[2]/div/input");
+	By cv=By.xpath("//*[@id=\"payment-form\"]/div[3]/div[1]/input");
+	By expiry=By.xpath("//*[@id=\"payment-form\"]/div[3]/div[2]/input");
+	By expiryyear=By.xpath("//*[@id=\"payment-form\"]/div[3]/div[3]/input");
+	By payorder=By.xpath("//*[@id=\"submit\"]");
+	By ordeplace=By.xpath("//*[@id=\"form\"]/div/div/div/h2/b");
+	By down=By.xpath("//*[@id=\"form\"]/div/div/div/a");
+	By con=By.xpath("//*[@id=\"form\"]/div/div/div/div/a");
+	
+	public Products(WebDriver driver)
 	{
-		WebDriver driver;
-		By product=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
-		By target = By.xpath("/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/a");
-		By cont=By.xpath("//*[@id=\"cartModal\"]/div/div/div[3]/button");
-		By view=By.xpath("/html/body/section[2]/div/div/div[2]/div/div[3]/div/div[2]/ul/li/a/i");
-		By arrowmark=By.xpath("//*[@id=\"quantity\"]");
-		By addcart=By.xpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div/span/button");
-		By viewc=By.xpath("//*[@id=\"cartModal\"]/div/div/div[2]/p[2]/a/u");
-		
+		this.driver=driver;	
+	}
+	
