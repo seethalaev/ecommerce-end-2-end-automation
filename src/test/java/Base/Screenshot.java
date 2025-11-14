505d8839f3d652b4b4ef52864e61ad1635c164c7
seethala e v <sheethalev@gmail.com>
2025-11-14 23:35:33 +0530

    screenshot,inspected

---
+++ src/test/java/Base/Screenshot.java
@@ -0,0 +1,18 @@
+package Base;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Screenshot 
+{
+	WebDriver driver;
+	By testcas=By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a");
+	By img=By.xpath("//*[@id=\"header\"]/div/div/div/div[1]/div/a/img");
+	
