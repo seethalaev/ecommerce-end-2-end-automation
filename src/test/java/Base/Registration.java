e5b8c9c4b2a29580c0ff1598d28f1c21ca1b149a
seethala e v <sheethalev@gmail.com>
2025-11-16 23:23:13 +0530

    saved new data

---
+++ src/test/java/Base/Registration.java
@@ -0,0 +1,31 @@
+package Base;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Registration 
+{
+	WebDriver driver;
+	
+	
+	By name=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]");
+	By email=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]");
+	By signup=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button");
+	
+	public Registration(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	public void Regitration(String nam,String mail)
+	{
+		driver.findElement(name).sendKeys(nam);
+		driver.findElement(email).sendKeys(mail);
+		driver.findElement(signup).click();
+	
+		
+
+	}
+	
+}
