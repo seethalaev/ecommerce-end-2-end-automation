343b21918aa62428461eb7713e777dddb6d16fce
seethala e v <sheethalev@gmail.com>
2025-11-20 23:16:19 +0530

    xpath is not correct

--- src/test/java/Base/Delete.java
+++ src/test/java/Base/Delete.java
@@ -7,3 +7,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class Delete
+{
+	WebDriver driver;
+	
+	
+	public Delete(WebDriver driver)
+	{
+		this.driver=driver;
+	}
+	public void delt()
+	{
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+	        wait.until(ExpectedConditions.elementToBeClickable(delete)).click();
+	        String url = driver.getCurrentUrl();
+	        System.out.println("After delete, current URL: " + url);
+	    }
+	
+	}
+	
+	    
+
+
