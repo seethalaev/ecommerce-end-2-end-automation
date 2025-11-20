07b1dfa03958a5057f0bfd816506363b0a14fd5c
seethala e v <sheethalev@gmail.com>
2025-11-20 23:15:53 +0530

    imported delte

---
+++ src/test/java/Base/Delete.java
@@ -0,0 +1,9 @@
+package Base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
