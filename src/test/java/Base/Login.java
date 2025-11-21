e9d3d4a15451c1e102bc78aa4d7e73bce751f16a
seethala e v <sheethalev@gmail.com>
2025-11-21 23:04:31 +0530

    wait added now

--- src/test/java/Base/Login.java
+++ src/test/java/Base/Login.java
@@ -28,6 +28,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 		public Login(WebDriver driver)
 		{
 			this.driver = driver;
+			wait=new WebDriverWait(driver,Duration.ofSeconds(10));
 
 		}
 	public void login(String mail,String pswd) throws IOException
